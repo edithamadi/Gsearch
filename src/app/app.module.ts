086abd0f13a281f0882cf35579a564f5efd38074
Edith <you@example.com>
2018-08-17 16:49:20 +0300

    modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -11,6 +12,7 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
