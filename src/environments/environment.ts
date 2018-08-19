7610224a9deeb0f74d9fac36dfea149c2b22201a
Edith <you@example.com>
2018-08-19 23:39:08 +0300

    updated enviroment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   myApi: '75de827c4d0ca6139b9b0c7e1a7139173b793ce9' 
 
 };
