5800ee8a5fa252bb3c96007d9e8a9c0ec64ca134
Edith <you@example.com>
2018-08-20 17:40:16 +0300

    added api keys to file environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  myApi: '75de827c4d0ca6139b9b0c7e1a7139173b793ce9' 
+  apiUrl:https://api.github.com/users/,
+  userapikey: '?access_token=75de827c4d0ca6139b9b0c7e1a7139173b793ce9' ,
+  repositoryapikey:'/repos?access_token=75de827c4d0ca6139b9b0c7e1a7139173b793ce9'
 
 };
 
