8d944843a43d7f1855733ed32d3dafb7da648de2
Edith <you@example.com>
2018-08-23 00:20:57 +0300

    added api keys and url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/',
+  userapikey: '?access_token=75de827c4d0ca6139b9b0c7e1a7139173b793ce9' ,
+  repositoryapikey:'/repos?access_token=75de827c4d0ca6139b9b0c7e1a7139173b793ce9'
+
 };
