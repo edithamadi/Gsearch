b8ca4c01d67dcf7dee05b40906c5bd99989b9007
Edith <you@example.com>
2018-08-23 00:46:46 +0300

    updated User.ts file

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,3 +1,4 @@
 export class User {
-    constructor(public name:string, public login: string, public avatar_url:string, public public_repos:number , public html_url: string,public created_at:Date, public followers:number ,public following:number){
-    }}
+    constructor(public name:string, public login: string, public avatar_url:string, public public_repos:number , public html_url: string, public followers:number ,public following:number,public repos_url:string){
+    }
+}
