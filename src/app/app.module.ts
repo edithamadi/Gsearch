e5ac3345017d8cfe15facacec9848ca76ec04d00
Edith <you@example.com>
2018-08-23 00:44:03 +0300

    import of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { UserService } from '../app/user.service';
+
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
    
     AppComponent,
-    HttpClientModule,
-    UserComponent
+    UserComponent,
+    RepoComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
